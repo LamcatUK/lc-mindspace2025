d4fad6c992ebb7da24efbfb0dfbdf453f1db4217
LamcatUK <git@lamcat.co.uk>
2026-02-21 12:24:42 +0000

    pink card lead

--- blocks/lc-four-cards.php
+++ blocks/lc-four-cards.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 			?>
 		<div class="col-md-6 col-lg-3">
 			<div class="four_cards__card">
-				<div class="four_cards__lead"><?= esc_html( get_field( 'lead_1' ) ); ?></div>
+				<div class="four_cards__lead has-pink-400-color"><?= esc_html( get_field( 'lead_1' ) ); ?></div>
 				<div class="four_cards__content"><?= esc_html( get_field( 'content_1' ) ); ?></div>
 				<div class="four_cards__attr"><?= esc_html( get_field( 'attr_1' ) ); ?></div>
 			</div>
@@ -26,7 +26,7 @@ defined( 'ABSPATH' ) || exit;
 			?>
 		<div class="col-md-6 col-lg-3">
 			<div class="four_cards__card">
-				<div class="four_cards__lead"><?= esc_html( get_field( 'lead_2' ) ); ?></div>
+				<div class="four_cards__lead has-pink-400-color"><?= esc_html( get_field( 'lead_2' ) ); ?></div>
 				<div class="four_cards__content"><?= esc_html( get_field( 'content_2' ) ); ?></div>
 				<div class="four_cards__attr"><?= esc_html( get_field( 'attr_2' ) ); ?></div>
 			</div>
@@ -37,7 +37,7 @@ defined( 'ABSPATH' ) || exit;
 			?>
 		<div class="col-md-6 col-lg-3">
 			<div class="four_cards__card">
-				<div class="four_cards__lead"><?= esc_html( get_field( 'lead_3' ) ); ?></div>
+				<div class="four_cards__lead has-pink-400-color"><?= esc_html( get_field( 'lead_3' ) ); ?></div>
 				<div class="four_cards__content"><?= esc_html( get_field( 'content_3' ) ); ?></div>
 				<div class="four_cards__attr"><?= esc_html( get_field( 'attr_3' ) ); ?></div>
 			</div>
@@ -48,7 +48,7 @@ defined( 'ABSPATH' ) || exit;
 			?>
 		<div class="col-md-6 col-lg-3">
 			<div class="four_cards__card">
-				<div class="four_cards__lead"><?= esc_html( get_field( 'lead_4' ) ); ?></div>
+				<div class="four_cards__lead has-pink-400-color"><?= esc_html( get_field( 'lead_4' ) ); ?></div>
 				<div class="four_cards__content"><?= esc_html( get_field( 'content_4' ) ); ?></div>
 				<div class="four_cards__attr"><?= esc_html( get_field( 'attr_4' ) ); ?></div>
 			</div>
